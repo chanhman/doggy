81f2aa44e790c853b15473116dc28b2053e11746
Chanh Nguyen <hello@chanh.design>
2021-06-25 14:30:40 -0600

    Add more styles

--- components/Doggies/index.tsx
+++ components/Doggies/index.tsx
@@ -14,10 +14,10 @@ export default function Doggies({
       {data.map((dog) => (
         <div className="card" key={dog.id} onClick={() => setCurrentDog(dog)}>
           <p>
-            <b>Name:</b> {dog.name ? dog.name : 'N/A'}
+            <div className="label">Name:</div> {dog.name ? dog.name : 'N/A'}
           </p>
           <p>
-            <b>Breed group:</b> {dog.breed_group ? dog.breed_group : 'N/A'}
+            <div className="label">Breed group:</div> {dog.breed_group ? dog.breed_group : 'N/A'}
           </p>
         </div>
       ))}
